9fc12ab55f5f25ccbe6130ec88c0fd889f4749d5
JLStack <stackjl@appstate.edu>
2015-03-30 21:17:48 -0400

    added PopulateDepartment.sql

---
+++ db/PopulateDepartment.sql
@@ -0,0 +1,279 @@
+INSERT INTO department (Name, U_ID)
+VALUES ('Accounting', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Anthropology', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Appalachian Studies', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Apparel Design & Merchandising', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Applied Music', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Arabic', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Art', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Astronomy', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Athletic Training', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Biology', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Business', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Business Education', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Chemistry', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Chinese', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Commercial Photography', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Communication', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Communication Sci & Disorders', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Computer Information Systems', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Computer Science', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Criminal Justice', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Curriculum and Instruction', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Dance', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Economics', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Educational Foundations', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Educational Leadership', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('English', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Entrepreneurship', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Environmental Science', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Exercise Science', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Family and Consumer Sciences', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Fermentation Sciences', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Finance, Banking and Insurance', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('French', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Gender, Women's & Sexuality St', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('General Science', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Geography', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Geology', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('German', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Global Studies', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Graphic Arts and Imaging Tech', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Health Care Management', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Health Education', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Health Promotion', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Health and Physicial Education', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Higher Education', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('History', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Honors', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Hospitality Management', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Human Devel and Psych Counsel', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Industrial Design', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Instructional Tech/Computers', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Interdisciplinary Studies', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Interior Design', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Japanese', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Languages, Literature, Culture', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Law', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Leadership in School Admin', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Library Science', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Management', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Marketing', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Master of Business Administrat', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Mathematics', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Military Sci and Leadership', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Music', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Nursing', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Nutrition', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Philosophy', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Physical Education', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Physics', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Planning', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Political Science', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Portuguese', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Production Operations Mgt', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Psychology', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Public Administration', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Reading', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Recreation Management', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Religious Studies', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Research', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Rhetoric and Composition', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Russian', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Social Work', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Sociology', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Spanish', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Special Education', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Statistics', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Supply Chain Management', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Sustainable Development', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Technology', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Theatre', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('University College', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('University Studies', 1);
+
+INSERT INTO department (Name, U_ID)
+VALUES ('Watauga Residential College', 1);
+
