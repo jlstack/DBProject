66036bf94e429a9292352f982ba6dba75ca3a0fc
Christopher Waldon <wordsmith@embarqmail.com>
2015-04-14 18:41:24 -0400

    Added database connection and some basic JQuery.

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <!DOCTYPE html>
+
+<?php
+    $mydb = @mysql_connect("localhost","mulfordjc","900495183");
+    
+    if (!$mydb) {
+        echo "Error connecting to database";
+        exit();
+    }
+
+    mysql_select_db("mulfordjc", $mydb);
+    ?>
+
 <html lang="en">
-<head>
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,16 +25,62 @@
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-</head>
-<body>
-<h1>Hello, world!</h1>
+  </head>
+  <body>
+	<nav class="navbar navbar-inverse">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Brand</a>
+	    </div>
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
-</body>
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+		<li class="dropdown">
+		  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+		  <ul class="dropdown-menu" role="menu">
+
+          <?php
+          $query_result = @mysql_query("SELECT * FROM employee",$mydb);
+
+          while ($row = mysql_fetch_array($query_result)) {
+              echo "<li><a href='#'>{$row['fname']}</a></li>";
+          }
+          ?>
+
+		  </ul>
+		</li>
+	      </ul>
+		</li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav> 
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+	console.log("hello.");
+	console.log($('ul.dropdown-menu').text());
+	var drop = $('ul.dropdown-menu li:last');
+	drop.after('<li><a href="#">Chris Waldon</a></li>');
+	$('ul.dropdown-menu li').on("click", function() {
+		$(this).parent().after('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a><ul class="dropdown-menu" role="menu"><li><a href="#">Chris Waldon</a></li></ul></li>');
+		//console.log(this.text);
+});
+	</script>
+  </body>
 </html>
